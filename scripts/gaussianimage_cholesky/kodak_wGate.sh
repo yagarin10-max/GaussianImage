dcb6a1c1f64a183b0d79c9b7a07faf9a35f37d07
yagarin10-max <rintoyagawa@keio.jp>
2025-12-22 11:42:25 +0100

    [add] add kodak_wGate.sh

---
+++ scripts/gaussianimage_cholesky/kodak_wGate.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+data_path=$1
+
+if [ -z "$data_path" ]; then
+    echo "Error: No data_path provided."
+    echo "Usage: $0 <data_path>"
+    exit 1
+fi
+
+for num_points in 393216 # 768x512
+do
+CUDA_VISIBLE_DEVICES=0 python train.py -d $data_path \
+--data_name kodak --model_name GaussianImage_Cholesky --num_points $num_points --iterations 50000 --save_imgs
+done
