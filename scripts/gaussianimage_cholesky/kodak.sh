84ce24f5b042e6bcd524c67768f1f7e5e812a9d8
yagarin10-max <rintoyagawa@keio.jp>
2026-01-20 12:07:24 +0100

    [exp] kodak 5k to 100k woclamp

--- scripts/gaussianimage_cholesky/kodak.sh
+++ scripts/gaussianimage_cholesky/kodak.sh
@@ -8,8 +8,8 @@ if [ -z "$data_path" ]; then
     exit 1
 fi
 
-for num_points in 50000
+for num_points in 5000 10000 15000 20000 25000 30000 35000 40000 45000 50000 55000 60000 65000 70000 75000 80000 85000 90000 95000 100000 393216
 do
 CUDA_VISIBLE_DEVICES=0 python train.py -d $data_path \
---data_name kodak_small --model_name GaussianImage_Cholesky --num_points $num_points --iterations 50000 --save_imgs --use_wandb
+--data_name kodak_small --model_name GaussianImage_Cholesky --num_points $num_points --iterations 50000 --save_imgs
 done
