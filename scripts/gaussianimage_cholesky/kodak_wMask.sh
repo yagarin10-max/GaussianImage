f9201381588bda7817675b0cda463f4677cc1bf8
yagarin10-max <rintoyagawa@keio.jp>
2026-01-21 18:41:28 +0100

    [exp] test GI and GImask on kodak

--- scripts/gaussianimage_cholesky/kodak_wMask.sh
+++ scripts/gaussianimage_cholesky/kodak_wMask.sh
@@ -8,8 +8,36 @@ if [ -z "$data_path" ]; then
     exit 1
 fi
 
-for num_points in 85000 # 768x512
+for num_points in 70000 # 768x512
 do
 CUDA_VISIBLE_DEVICES=0 python train.py -d $data_path \
---data_name kodak_small --model_name GaussianImage_Cholesky_wMask --num_points $num_points --iterations 50000 --save_imgs --start_mask_training 15000 --stop_mask_training 40000 --use_wandb
+--data_name kodak \
+--model_name GaussianImage_Cholesky_wMask \
+--num_points $num_points \
+--iterations 50000 \
+--save_imgs \
+--start_mask_training 5000 \
+--stop_mask_training 10000 \
+--use_wandb \
+--reg_type kl \
+--target_sparsity 0.7 \
+--lambda_reg 0.005 \
+--init_mask_logit 2.0
+done
+
+for num_points in 70000 # 768x512
+do
+CUDA_VISIBLE_DEVICES=0 python train.py -d $data_path \
+--data_name kodak \
+--model_name GaussianImage_Cholesky_wMask \
+--num_points $num_points \
+--iterations 50000 \
+--save_imgs \
+--start_mask_training 5000 \
+--stop_mask_training 10000 \
+--use_wandb \
+--reg_type kl \
+--target_sparsity 0.7 \
+--lambda_reg 0.005 \
+--init_mask_logit 1.0
 done
