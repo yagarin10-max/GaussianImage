cdd6295dfc5e17d23a756d9796dea372e4362a2a
yagarin10-max <rintoyagawa@keio.jp>
2026-01-22 20:07:37 +0100

    [exp] GI_70k

--- scripts/gaussianimage_cholesky/kodak.sh
+++ scripts/gaussianimage_cholesky/kodak.sh
@@ -8,7 +8,7 @@ if [ -z "$data_path" ]; then
     exit 1
 fi
 
-for num_points in 90000
+for num_points in 70000
 do
 CUDA_VISIBLE_DEVICES=0 python train.py -d $data_path \
 --data_name kodak --model_name GaussianImage_Cholesky --num_points $num_points --iterations 50000 --save_imgs
